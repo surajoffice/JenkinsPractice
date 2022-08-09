37ed763d4a939af360e10dfeff14db2d740c91d9
surajoffice <110885270+surajoffice@users.noreply.github.com>
2022-08-09 11:19:02 +0530

    Create Hello program

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String a[])
+	{ System.out.println("This file was modified");
+		for(int i=1;i<=10;i++)
+		{
+		  System.out.println("Hello world");
+		}
+	}
+}
